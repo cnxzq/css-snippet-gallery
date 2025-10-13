92115df856f1cd3321e1ff47bfad6540cf7a0501
zq <290353142@qq.com>
2025-10-13 17:16:15 +0800

    init

--- scripts/gen-html.js
+++ scripts/gen-html.js
@@ -4,5 +4,5 @@ const path = require('path')
 const jsonStr = fs.readFileSync(path.join(__dirname,'html-structure.json'),'utf-8')
 const html = fs.readFileSync(path.join(__dirname,'index-template.html'),'utf-8')
 
-fs.writeFileSync(path.join(__dirname,'../examples/index.html'),html.replace('/*"DATA-PLACEHOLDER"*/[]/*"DATA-PLACEHOLDER"*/',JSON.stringify(JSON.parse(jsonStr))))
+fs.writeFileSync(path.join(__dirname,'../docs/index.html'),html.replace('/*"DATA-PLACEHOLDER"*/[]/*"DATA-PLACEHOLDER"*/',JSON.stringify(JSON.parse(jsonStr))))
 
